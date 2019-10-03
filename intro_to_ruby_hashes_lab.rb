f976fca4f136fd3106b6e0b5e0b43af9616c0f30
William <teststuffreally@gmail.com>
2019-10-03 15:12:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,10 +34,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash[key]=1
+ if (condition)
+ hash[key]=1
  return hash
  hash.key(1)
- if()
     hash{key] = 1
  end
  
